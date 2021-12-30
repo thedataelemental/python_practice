e6ffddbe9c674f1361bfdc32a1cdb7641f80dd08
Data <95385516+thedataelemental@users.noreply.github.com>
2021-12-29 18:44:32 -0800

    Add files via upload

--- PG_Tetris/pg_tetris.py
+++ PG_Tetris/pg_tetris.py
@@ -11,12 +11,17 @@ import random
 
 # Falling "tetris block" / "tetris piece". Tiles arranged in a shape
 class Block:
-	def __init__(self, tile, shape):
-		self.tile = tile  # Image repeated for each tile of the block
-		self.shape = shape  # Shape of block (2D array of 0's and 1's)
+	def __init__(self, tile, shapes):
+		# Image repeated for each tile of the block
+		self.tile = tile 
+		# List of shapes (rotations) of block (2D arrays of 0's and 1's)
+		self.shapes = shapes  
 		# Starting position at top of screen
 		self.x = 240
 		self.y = 64
+		# Default shape / rotation
+		self.shape_index = 0
+		self.current_shape = self.shapes[self.shape_index]
 
 
 # Draw background and blocks
@@ -40,12 +45,12 @@ def render_screen():
 	# Draw falling block
 	render_x = current_block.x
 	render_y = current_block.y
-	for row in current_block.shape:
+	for row in current_block.current_shape:
 		for bit in row:
 			if bit == 1:
 				screen.blit(current_block.tile, (render_x, render_y))
 			render_x += 16
-		render_x = 240
+		render_x = current_block.x
 		render_y += 16
 		
 	pygame.display.flip()
@@ -72,6 +77,7 @@ text_font = pygame.font.Font("NESfont.ttf", 14)
 WHITE = (255, 255, 255)
 lines = 0
 level = 1
+frame_counter = 0
 
 # Define block shapes
 # Shapes are represented internally as 4x4 arrays of 0's and 1's.
@@ -84,12 +90,60 @@ O_shape_1 = [
 	[0, 1, 1, 0],
 	]
 
+O_shape_2 = [
+	[0, 0, 0, 0],
+	[0, 0, 0, 0],
+	[0, 1, 1, 0],
+	[0, 1, 1, 0],
+	]
+	
+O_shape_3 = [
+	[0, 0, 0, 0],
+	[0, 0, 0, 0],
+	[0, 1, 1, 0],
+	[0, 1, 1, 0],
+	]
+	
+O_shape_4 = [
+	[0, 0, 0, 0],
+	[0, 0, 0, 0],
+	[0, 1, 1, 0],
+	[0, 1, 1, 0],
+	]
+	
+O_shapes = [O_shape_1, O_shape_2, O_shape_3, O_shape_4]
+
+
 I_shape_1 = [
 	[0, 1, 0, 0],
 	[0, 1, 0, 0],
 	[0, 1, 0, 0],
 	[0, 1, 0, 0],
 	]
+	
+I_shape_2 = [
+	[0, 0, 0, 0],
+	[0, 0, 0, 0],
+	[1, 1, 1, 1],
+	[0, 0, 0, 0],
+	]
+
+I_shape_3 = [
+	[0, 1, 0, 0],
+	[0, 1, 0, 0],
+	[0, 1, 0, 0],
+	[0, 1, 0, 0],
+	]
+	
+I_shape_4 = [
+	[0, 0, 0, 0],
+	[0, 0, 0, 0],
+	[1, 1, 1, 1],
+	[0, 0, 0, 0],
+	]
+
+I_shapes = [I_shape_1, I_shape_2, I_shape_3, I_shape_4]
+
 
 T_shape_1 = [
 	[0, 0, 0, 0],
@@ -97,6 +151,30 @@ T_shape_1 = [
 	[0, 1, 0, 0],
 	[1, 1, 1, 0],
 	]
+	
+T_shape_2 = [
+	[0, 0, 0, 0],
+	[0, 1, 0, 0],
+	[0, 1, 1, 0],
+	[0, 1, 0, 0],
+	]
+
+T_shape_3 = [
+	[0, 0, 0, 0],
+	[0, 0, 0, 0],
+	[1, 1, 1, 0],
+	[0, 1, 0, 0],
+	]
+
+T_shape_4 = [
+	[0, 0, 0, 0],
+	[0, 1, 0, 0],
+	[1, 1, 0, 0],
+	[0, 1, 0, 0],
+	]
+	
+T_shapes = [T_shape_1, T_shape_2, T_shape_3, T_shape_4]
+	
 
 J_shape_1 = [
 	[0, 0, 0, 0],
@@ -105,6 +183,29 @@ J_shape_1 = [
 	[1, 1, 0, 0],
 	]
 	
+J_shape_2 = [
+	[0, 0, 0, 0],
+	[1, 0, 0, 0],
+	[1, 1, 1, 0],
+	[0, 0, 0, 0],
+	]
+	
+J_shape_3 = [
+	[0, 0, 0, 0],
+	[0, 1, 1, 0],
+	[0, 1, 0, 0],
+	[0, 1, 0, 0],
+	]
+	
+J_shape_4 = [
+	[0, 0, 0, 0],
+	[0, 0, 0, 0],
+	[1, 1, 1, 0],
+	[0, 0, 1, 0],
+	]
+	
+J_shapes = [J_shape_1, J_shape_2, J_shape_3, J_shape_4]
+	
 	
 L_shape_1 = [
 	[0, 0, 0, 0],
@@ -112,6 +213,30 @@ L_shape_1 = [
 	[0, 1, 0, 0],
 	[0, 1, 1, 0],
 	]
+	
+L_shape_2 = [
+	[0, 0, 0, 0],
+	[0, 0, 0, 0],
+	[1, 1, 1, 0],
+	[1, 0, 0, 0],
+	]
+	
+L_shape_3 = [
+	[0, 0, 0, 0],
+	[1, 1, 0, 0],
+	[0, 1, 0, 0],
+	[0, 1, 0, 0],
+	]
+	
+L_shape_4 = [
+	[0, 0, 0, 0],
+	[0, 0, 1, 0],
+	[1, 1, 1, 0],
+	[0, 0, 0, 0],
+	]
+
+L_shapes = [L_shape_1, L_shape_2, L_shape_3, L_shape_4]
+
 
 S_shape_1 = [
 	[0, 0, 0, 0],
@@ -120,12 +245,60 @@ S_shape_1 = [
 	[0, 0, 1, 0],
 	]
 
+S_shape_2 = [
+	[0, 0, 0, 0],
+	[0, 0, 0, 0],
+	[0, 1, 1, 0],
+	[1, 1, 0, 0],
+	]
+	
+S_shape_3 = [
+	[0, 0, 0, 0],
+	[0, 1, 0, 0],
+	[0, 1, 1, 0],
+	[0, 0, 1, 0],
+	]
+	
+S_shape_4 = [
+	[0, 0, 0, 0],
+	[0, 0, 0, 0],
+	[0, 1, 1, 0],
+	[1, 1, 0, 0],
+	]
+
+S_shapes = [S_shape_1, S_shape_2, S_shape_3, S_shape_4]
+
+
 Z_shape_1 = [
 	[0, 0, 0, 0],
 	[0, 0, 1, 0],
 	[0, 1, 1, 0],
 	[0, 1, 0, 0],
 	]
+	
+Z_shape_2 = [
+	[0, 0, 0, 0],
+	[0, 0, 0, 0],
+	[1, 1, 0, 0],
+	[0, 1, 1, 0],
+	]
+
+Z_shape_3 = [
+	[0, 0, 0, 0],
+	[0, 0, 1, 0],
+	[0, 1, 1, 0],
+	[0, 1, 0, 0],
+	]
+	
+Z_shape_4 = [
+	[0, 0, 0, 0],
+	[0, 0, 0, 0],
+	[1, 1, 0, 0],
+	[0, 1, 1, 0],
+	]
+	
+Z_shapes = [Z_shape_1, Z_shape_2, Z_shape_3, Z_shape_4]
+	
 
 fill_well_shape = [
 	[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -202,13 +375,13 @@ block_tiles = [
 	]
 
 block_shapes = [
-	O_shape_1,
-	I_shape_1,
-	T_shape_1,
-	J_shape_1,
-	L_shape_1,
-	S_shape_1,
-	Z_shape_1,
+	O_shapes,
+	I_shapes,
+	T_shapes,
+	J_shapes,
+	L_shapes,
+	S_shapes,
+	Z_shapes,
 	]
 	
 
@@ -220,13 +393,68 @@ block_shapes = [
 
 
 new_block()
+action_frame = 0
 
 # Main game loop
 while True:
-	pygame.event.pump()
+	# Quit game if window is closed
+	events = pygame.event.get()
+	for event in events:
+		if event.type == pygame.QUIT:
+			sys.exit()
+	
+	# Rotate block left and right
+	key = pygame.key.get_pressed()
+	if key[pygame.K_e]:
+		if done_action == False:
+			current_block.shape_index += 1
+			if current_block.shape_index >= 4:
+				current_block.shape_index = 0
+			current_block.current_shape = \
+				current_block.shapes[current_block.shape_index]
+			done_action = True
+			action_frame = frame_counter
+			
+	elif key[pygame.K_r]:
+		if done_action == False:
+			current_block.shape_index -= 1
+			if current_block.shape_index <= -1:
+				current_block.shape_index = 3
+			current_block.current_shape = \
+				current_block.shapes[current_block.shape_index]
+			done_action = True
+			action_frame = frame_counter
+	
+	# Move block left and right
+	elif key[pygame.K_LEFT] or key[pygame.K_a]:
+		if (done_action == False) and (current_block.x > 176):
+			current_block.x -= 16
+			done_action = True
+			action_frame = frame_counter
+	
+	elif key[pygame.K_RIGHT] or key[pygame.K_d]:
+		if (done_action == False) and (current_block.x < 320):
+			current_block.x += 16
+			done_action = True
+			action_frame = frame_counter
+	
 	render_screen()
-	clock.tick(5 * fall_speed)
-	current_block.y +=16
+	clock.tick(60)
+	
+	frame_counter += 1
+	
+	# Wait a few frames before block can be rotated again
+	if abs(frame_counter - action_frame) >= 10:
+		done_action = False
+	
+	# Make blocks fall
+	if frame_counter == (10 / level):
+		current_block.y +=16
+		
+	if frame_counter >= 60:
+		frame_counter = 0
+	
+	# Make new block
 	if current_block.y == 336:
 		new_block()
 
